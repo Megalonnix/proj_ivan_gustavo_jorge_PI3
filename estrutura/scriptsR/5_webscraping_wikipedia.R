a63b8289b882c59fbdd1ddeca137297272a1a2d7
Megalonnix <149280452+Megalonnix@users.noreply.github.com>
2026-09-12 03:11:03 -0300

    baixando MANUALMENTE csv da wikipedia via scraping

--- estrutura/scriptsR/5_webscraping_wikipedia.R
+++ estrutura/scriptsR/5_webscraping_wikipedia.R
@@ -105,18 +105,13 @@ search_wikipedia_news <- function(query, top_n = 3,
 #   top_n = 3
 # )
 
-# source("https://raw.githubusercontent.com/Megalonnix/proj_ivan_gustavo_jorge_PI3/main/estrutura/scriptsR/5_webscraping_wikipedia.R")
+
+# BAIXANDO MANUALMENTE OS DADOS DA WIKIPEDIA!!!
+
 # resultados <- search_wikipedia_news("porto e economia de Santos", top_n = 3)
-# salvar_wikipedia_csv(resultados$dataframe)
 
-pacman::p_load(rvest, stringr, here)
+# URL_DESTINO_CSV_SCRAPING <- "C:/Users/Ivan/Documents/Pasta-Documentos-PC-antigo/GITHUB-Meus-Repositorios/PesquisaPI3_2026_v2/proj_ivan_gustavo_jorge(PI3)/estrutura/bancoDeDados"
 
-salvar_wikipedia_csv <- function(df, output_dir = here::here("estrutura", "bancoDeDados")) {
-  if (!dir.exists(output_dir)) {
-    dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
-  }
-  filename <- file.path(output_dir, "wikipedia_baixada_santista.csv")
-  write.csv(df, filename, row.names = FALSE, fileEncoding = "UTF-8")
-  cat(sprintf("\n>>> CSV salvo em: %s\n", normalizePath(filename)))
-  filename
-}
+# salvar_wikipedia_csv(
+#   resultados$dataframe, 
+#   output_dir = URL_DESTINO_CSV_SCRAPING)
